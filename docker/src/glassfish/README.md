d34f482bd717e1c242d0ba06c956de54e47a223e
Tobias <Tobias.Hassberg@web.de>
2016-12-29 21:33:30 +0100

    changed naming, dockerfile for importing glassfish folder from git into the image, image includes java, glassfish, git, ubuntu

---
+++ docker/src/glassfish/README.md
@@ -0,0 +1,4 @@
+Defaul Glassfish package with the recommented domains web, register, store, adapter, enterprise
+
+
+source: https://glassfish.java.net/download.html
---
+++ docker/src/startdomain.sh
@@ -0,0 +1,5 @@
+#script for startingt the domains
+
+usr/src/glassfish/glassfish4/bin/asadmin start-domain web
+usr/src/glassfish/glassfish4/bin/asadmin start-domain store
+
