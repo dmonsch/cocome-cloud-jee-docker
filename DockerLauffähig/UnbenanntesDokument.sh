faedea0d1bae3afdd98a252447b44cf774ac20c1
Benkler <benklerniko@t-online.de>
2017-01-04 22:19:13 +0100

    delete unnecessary file

--- DockerLauffähig/UnbenanntesDokument.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "Test"
-ADMIN_PORT=8048
-touch /usr/src/glassfish/glassfish4/glassfish/passwordfile
-
-echo "AS_ADMIN_PASSWORD=password" > /usr/src/glassfish/glassfish4/glassfish/passwordfile
