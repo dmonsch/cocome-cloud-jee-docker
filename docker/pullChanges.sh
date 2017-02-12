a7794d27574ae522dad498bf7fdb5e017941d3d3
tobias <tobias.hassberg@web.de>
2017-02-12 17:19:20 +0100

    reoved restart domains

--- docker/pullChanges.sh
+++ docker/pullChanges.sh
@@ -7,7 +7,7 @@ echo '####################  pull changes   ####################### '
 
 cd /usr/src/cocome/cocome-maven-project git checkout master && git pull
 
-#cd /usr/src/serviceadapter git pull
+cd /usr/src/serviceadapter git pull
 
 echo '####################  pulled changes   ####################### '
 
@@ -16,33 +16,33 @@ cd /usr/src/cocome/cocome-maven-project && mvn -s /usr/src/cocome-maven-project-
 cd /usr/src/cocome/cocome-maven-project && mvn -s /usr/src/cocome-maven-project-setting.xml install
 
 
-#cd /usr/src/serviceadapter && mvn -s /usr/src/serviceadapter-settings.xml clean post-clean 
+cd /usr/src/serviceadapter && mvn -s /usr/src/serviceadapter-settings.xml clean post-clean 
 
-#cd /usr/src/serviceadapter && mvn -s /usr/src/serviceadapter-settings.xml install
+cd /usr/src/serviceadapter && mvn -s /usr/src/serviceadapter-settings.xml install
 
 
 ##############################################################################
 #restart needed because of changed attributes like password
 #Important notice: restart of the glassfish domains or start in general has to take playce in this order!
-echo '########## restart database ##################'
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-database 
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-database 
+#echo '########## restart database ##################'
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-database 
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-database 
 
-echo '########## restart domain REGISTRY ##################'
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain registry
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-domain registry
+#echo '########## restart domain REGISTRY ##################'
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain registry
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-domain registry
 
-echo '########## restart domain ADAPTER ##################'
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain adapter
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-domain  adapter
+#echo '########## restart domain ADAPTER ##################'
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain adapter
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-domain  adapter
 
-echo '########## restart domain WEB ##################'
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain web
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-domain  web
+#echo '########## restart domain WEB ##################'
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain web
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-domain  web
 
-echo '########## restart domain STORE ##################'
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain store
-/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-domain  store
+#echo '########## restart domain STORE ##################'
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain store
+#/usr/src/glassfish/glassfish4/glassfish/bin/asadmin start-domain  store
 
 #echo '########## restart domain ENTERPRISE ##################'
 #/usr/src/glassfish/glassfish4/glassfish/bin/asadmin stop-domain enterprise
